9de9e118c42997ada55417fa0e159bc0e3414cbf
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-02-22 22:45:10 +0600

    route fixed

---
+++ routes/UserRoute.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const router = express.Router();
+
+// Import the user controller
+const { RegisterUser, LoginUser } = require("../controllers/UserController");
+
+// Import the avatar-upload middleware
+const { uploadAvatar } = require("../middlewares/AvatarUploadMiddleware");
+
+// Define the user routes
+
+/**
+ * POST /register
+ * Uploads the user's avatar using the 'uploadAvatar' middleware
+ * and registers the user using the RegisterUser controller function.
+ */
+router.route("/register").post(uploadAvatar, RegisterUser);
+
+/**
+ * POST /login
+ * Logs in the user using the LoginUser controller function.
+ */
+router.route("/login").post(LoginUser);
+
+// Export the router
+module.exports = router;
