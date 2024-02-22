17329133a8377f6694e9609d099028b03b3ecf42
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-02-22 22:31:22 +0600

    User schema added

---
+++ models/UserModel.js
@@ -0,0 +1,49 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const userSchema = new mongoose.Schema({
+  first_name: {
+    type: String,
+    required: true,
+  },
+  last_name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  wishlist: {
+    type: [
+      {
+        productId: { type: mongoose.Schema.Types.ObjectId, ref: "Product" },
+        addedAt: { type: Date, default: Date.now },
+      },
+    ],
+    default: [],
+  },
+  cart: {
+    type: [
+      {
+        productId: { type: mongoose.Schema.Types.ObjectId, ref: "Product" },
+        quantity: { type: Number, default: 1 },
+        addedAt: { type: Date, default: Date.now },
+      },
+    ],
+    default: [],
+  },
+});
+
+userSchema.pre("save", async function (next) {
+  if (this.isModified("password")) {
+    this.password = await bcrypt.hash(this.password, 8);
+  }
+  next();
+});
+
+module.exports = mongoose.model("User", userSchema);
