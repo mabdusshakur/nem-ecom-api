4887140d264d128c547a3bb761f404d9221ae5a4
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-02-23 11:55:39 +0600

    getProfile routes added with multer urlencoders ...

--- routes/UserRoute.js
+++ routes/UserRoute.js
@@ -1,12 +1,20 @@
 const express = require("express");
 const router = express.Router();
 
+// Use multer to handle form-data body values
+const multer = require("multer");
+const upload = multer();
+
 // Import the user controller
-const { RegisterUser, LoginUser } = require("../controllers/UserController");
+const {
+  RegisterUser,
+  LoginUser,
+  GetProfile,
+} = require("../controllers/UserController");
 
 // Import the avatar-upload middleware
 const { uploadAvatar } = require("../middlewares/AvatarUploadMiddleware");
-const { AuthMiddleware }  = require("../middlewares/AuthMiddleware");
+const { AuthMiddleware } = require("../middlewares/AuthMiddleware");
 
 // Define the user routes
 
@@ -21,7 +29,13 @@ router.route("/register").post(uploadAvatar, RegisterUser);
  * POST /login
  * Logs in the user using the LoginUser controller function.
  */
-router.route("/login").post(LoginUser);
+router.route("/login").post(upload.none(), LoginUser);
+
+/**
+ * GET /profile
+ * Returns the user's profile
+ */
+router.route("/profile").get(upload.none(), AuthMiddleware, GetProfile);
 
 // Export the router
 module.exports = router;
