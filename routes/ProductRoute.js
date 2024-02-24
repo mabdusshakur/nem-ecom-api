4322844f98081f69048b74704a2d15b9aecff946
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-02-24 10:54:11 +0600

    product route defined

---
+++ routes/ProductRoute.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const router = express.Router();
+
+// Use multer to handle form-data body values
+const multer = require("multer");
+const upload = multer();
+
+// Import the product controller
+const {
+  getAllProducts,
+  getProductById,
+  createProduct,
+  updateProduct,
+  deleteProduct,
+  deleteProductImage,
+} = require("../controllers/ProductController");
+
+// Import the product-image-upload middleware
+const { uploadImages } = require("../middlewares/ProductImageUploadMiddleware");
+
+
+// Define the product routes
+
+/**
+ * GET /products
+ * Returns all products
+ */
+router.route("/products").get(getAllProducts);
+
+/**
+ * GET /products/:id
+ * Returns a single product by ID
+ */
+router.route("/products/:id").get(getProductById);
+
+/**
+ * POST /products
+ * Creates a new product
+ */
+router.route("/products").post(uploadImages, createProduct);
+
+/**
+ * PUT /products/:id
+ * Updates a product by ID
+ */
+router.route("/products/:id").put(uploadImages, updateProduct);
+
+/**
+ * DELETE /products/:id
+ * Deletes a product by ID
+ */
+router.route("/products/:id").delete(deleteProduct);
+
+/**
+ * DELETE /products/:id/images/:index
+ * Deletes a product image by image index
+ */
+router.route("/products/:id/images/:index").delete(deleteProductImage);
+
+// Export the router
+module.exports = router;
