0d32d7b6e08efcd694f06f9c680615d44c6e2541
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-02-25 11:30:53 +0600

    commented

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// Import required modules
 require("dotenv").config();
 const express = require("express");
 const db_connect = require("./database/db");
@@ -7,13 +8,21 @@ const app = express();
 const port = process.env.SERVER_PORT;
 const host = process.env.SERVER_HOST;
 
-app.use('/uploads', express.static('uploads'));
+// Serve static files from the "uploads" directory
+app.use("/uploads", express.static("uploads"));
 
+// Use UserRouter for handling user-related routes
 app.use("/api/v1/users", UserRouter);
 
+/**
+ * Function to start the server
+ */
 const startServer = async () => {
   try {
+    // Connect to the database
     await db_connect();
+
+    // Start the server
     app.listen(port, () => {
       console.log(`Server listening at ${host}:${port}`);
     });
@@ -22,4 +31,5 @@ const startServer = async () => {
   }
 };
 
+// Call the startServer function to start the server
 startServer();
