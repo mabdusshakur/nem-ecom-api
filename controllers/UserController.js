aed8fda4ecd6085c5d745e0e3e281055d46be208
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-03-03 00:33:01 +0600

    UserController.js

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -42,7 +42,7 @@ const LoginUser = async (req, res) => {
   } catch (err) {
     res.status(500).json({
       status: false,
-      message: "Internal Server Error",
+      message: "Internal Server Error !",
       error: err.message,
     });
   }
