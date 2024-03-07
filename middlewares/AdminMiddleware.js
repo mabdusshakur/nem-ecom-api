b4744089ac5bf33a79914e087fe04fec2772d5e9
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-03-07 16:58:40 +0600

    isAdmin middleware added

--- middlewares/AdminMiddleware.js
+++ middlewares/AdminMiddleware.js
@@ -1,8 +1,10 @@
 const isAdmin = (req, res, next) => {
-    if (req.user.role !== "admin") {
-        return res.status(403).json({ error: "Forbidden, You dont have Admin Role." });
-    }
-    next();
+  if (req.user.role !== "admin") {
+    return res.status(403).json({
+      error: "Forbidden, You dont have Admin Role.",
+    });
+  }
+  next();
 };
 
 module.exports = { isAdmin };
