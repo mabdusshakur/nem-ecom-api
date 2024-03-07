0e709999d5f6930fedc7e20e5b288644066d85ce
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-03-07 16:54:04 +0600

    catch err handled

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -7,8 +7,8 @@ const getAllProducts = async (req, res) => {
   try {
     const products = await Product.find();
     res.status(200).json(products);
-  } catch (error) {
-    res.status(500).json({ error: "Internal server error" });
+  } catch (err) {
+    res.status(500).json({ message: "Internal server error", error: err.message});
   }
 };
 
