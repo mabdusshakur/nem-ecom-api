90322da3632ef0a50865548690bfd0f37cbb6d1b
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-03-09 14:31:18 +0600

    User model imported

--- controllers/CartController.js
+++ controllers/CartController.js
@@ -1,5 +1,5 @@
 const Product = require("../models/ProductModel");
-
+const User = require("../models/UserModel");
 
 const AddToCart = async (req, res) => {
   res.status(200).json({ message: "Added to cart" });
