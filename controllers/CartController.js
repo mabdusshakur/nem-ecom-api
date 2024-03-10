3895d1d434eb89698d2708c828fbf4f9f08f90f3
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-03-10 14:33:22 +0600

    addToCart func added

--- controllers/CartController.js
+++ controllers/CartController.js
@@ -2,6 +2,36 @@ const Product = require("../models/ProductModel");
 const User = require("../models/UserModel");
 
 const AddToCart = async (req, res) => {
-  res.status(200).json({ message: "Added to cart" });
+  try {
+    const { productId } = req.body;
+    const user = await User.findById(req.user._id);
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    const product = await Product.findById(productId);
+
+    if (!product) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+
+    const CartItem = {
+      productId: product._id,
+      quantity: 1,
+      addedAt: Date.now(),
+    };
+
+    user.cart.push(CartItem);
+    await user.save();
+
+    res.status(200).json({ message: "Added to cart" });
+  } catch (error) {
+    res.status(500).json({
+      status: false,
+      message: "Internal Server Error!",
+      error: error.message,
+    });
+  }
 };
 module.exports = { AddToCart };
