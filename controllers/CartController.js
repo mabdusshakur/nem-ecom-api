bd53054a90738e8372240269b9aa5b83160f6533
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-03-14 22:28:02 +0600

    all-cart-item func code added

--- controllers/CartController.js
+++ controllers/CartController.js
@@ -37,7 +37,20 @@ const AddToCart = async (req, res) => {
 
 // Get all cart items of user
 const allCartItems = async (req, res) => {
- 
+  try {
+    const user = await User.findById(req.user._id).populate("cart.productId");
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    res.status(200).json({ cart: user.cart });
+  }
+  catch (error) {
+    res.status(500).json({
+      status: false,
+      message: "Internal Server Error!",
+      error: error.message,
+    });
+  }
 }
 
 module.exports = { AddToCart };
