7d876a42fba140f35f6b43d57ad8da0735853868
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-03-15 14:12:04 +0600

    wishlist controller removed

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -58,16 +58,4 @@ const GetProfile = async (req, res) => {
   res.status(200).json(req.user);
 };
 
-const AddToWishlist = async (req, res) => {
-  try {
-    res.status(200).json({ message: "Added to wishlist" });
-  } catch (error) {
-    res.status(500).json({
-      status: false,
-      message: "Internal Server Error !",
-      error: err.message,
-    });
-  }
-};
-
 module.exports = { RegisterUser, LoginUser, GetProfile, AddToWishlist };
