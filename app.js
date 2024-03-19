dcc1ead949c98dc8ce731d1b7ff4a6c5c461f59f
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-03-19 14:20:21 +0600

    wishlist router exposed

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require("express");
 const db_connect = require("./database/db");
 const UserRouter = require("./routes/UserRoute");
 const ProductRouter = require("./routes/ProductRoute");
+const WishlistRouter = require("./routes/WishlistRoute");
 
 const app = express();
 const port = process.env.SERVER_PORT;
@@ -18,6 +19,8 @@ app.use("/api/v1/users", UserRouter);
 // Use ProductRouter for handling product-related routes
 app.use("/api/v1/products", ProductRouter);
 
+// Use WishlistRouter for handling wishlist-related routes
+app.use("/api/v1/wishlist", WishlistRouter);
 
 /**
  * Function to start the server
