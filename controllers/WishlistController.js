62d92845a429519679bdbabb73a1ffa2ead76e66
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-03-21 15:23:04 +0600

    wishlist function removed

--- controllers/WishlistController.js
+++ controllers/WishlistController.js
@@ -1,13 +1,5 @@
 const AddToWishlist = async (req, res) => {
-  try {
-    res.status(200).json({ message: "Added to wishlist" });
-  } catch (error) {
-    res.status(500).json({
-      status: false,
-      message: "Internal Server Error !",
-      error: err.message,
-    });
-  }
+  
 };
 
 module.exports = { AddToWishlist };
