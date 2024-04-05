69d2389e0751694331ce968273bba5d9ec416b50
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-04-05 14:27:59 +0600

    auth middleware used

--- routes/CartRouter.js
+++ routes/CartRouter.js
@@ -4,16 +4,14 @@ const router = express.Router();
 const multer = require("multer");
 const upload = multer();
 
-const {
-  AddToCart,
-} = require("../controllers/CartController");
+const { AddToCart } = require("../controllers/CartController");
 const { AuthMiddleware } = require("../middlewares/AuthMiddleware");
 
 /**
  * POST /carts
  * Add to cart
  */
-router.route("/").post(upload.none(), AddToCart);
+router.route("/").post(upload.none(), AuthMiddleware, AddToCart);
 
 // Export the router
 module.exports = router;
